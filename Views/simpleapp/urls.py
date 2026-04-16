1d91cc9bd7e6ba12ac541b96382abb649c49ba37
Ed <44eduard44@gmail.com>
2026-04-16 16:15:27 +0300

    'Update'

--- Views/simpleapp/urls.py
+++ Views/simpleapp/urls.py
@@ -3,5 +3,5 @@ from .views import ProductsList, ProductDetail
 
 urlpatterns = [
     path('', ProductsList.as_view()),
-    path('<int:pk>', ProductDetail.as_view()),
+    path('<int:pk>/', ProductDetail.as_view()),
 ]
--- Views/simpleapp/views.py
+++ Views/simpleapp/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -7,10 +8,18 @@ class ProductsList(ListView):
     template_name = 'flatpages/products.html'
     context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в пятницу"
+        return context
+
 class ProductDetail(DetailView):
     model = Product
     template_name = 'flatpages/product.html'
     context_object_name = 'product'
+
+
 
     
 
